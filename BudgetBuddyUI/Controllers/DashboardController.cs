68446b91f6fe221c10f2845d17187b4da38801b0
Alma <alma@alma-c-box.local>
2023-02-27 20:14:22 -0600

    Begin Working on the Budget Management Dashboard Page

--- BudgetBuddyUI/Controllers/DashboardController.cs
+++ BudgetBuddyUI/Controllers/DashboardController.cs
@@ -1,14 +1,66 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BudgetBuddyLibrary;
+using BudgetBuddyLibrary.Models;
+using BudgetBuddyUI.Areas.Identity.Data;
+using BudgetBuddyUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetBuddyUI.Controllers
 {
     public class DashboardController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _config;
+        private readonly ILogger<HomeController> _logger;
+
+        public DashboardController(UserManager<ApplicationUser> userManager,
+            IConfiguration config,
+            ILogger<HomeController> logger)
         {
-            return View();
+            _userManager = userManager;
+            _config = config;
+            _logger = logger;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            // Figure out who is logged in, and get the AspNetUserId for identifying the user
+            // in the BudgetDataDb
+            var aspNetUserId = await _userManager.GetUserIdAsync(await _userManager.GetUserAsync(User));
+
+            SqlDataTranslator sqlDataTranslator = new SqlDataTranslator();
+
+            // Get the logged in user's Id from the BudgetDataDb
+            int loggedInUserId = await sqlDataTranslator.GetUserIdByAspNetUserId(aspNetUserId,
+                _config.GetConnectionString("BudgetDataDbConnectionString"));
+
+            // Now, based on the logged in user Id, we need to get the default
+            // budget, if any, from the UsersBudgetNames table
+            List<UsersBudgetNamesModel> usersBudgetNames =
+                await sqlDataTranslator.GetUsersBudgetNamesRowsByUserId(loggedInUserId,
+                _config.GetConnectionString("BudgetDataDbConnectionString"));
+
+            List<DashboardBudgetsTableModel> dashboardBudgetsTableModel = new List<DashboardBudgetsTableModel>();
+
+            foreach (UsersBudgetNamesModel ubnm in usersBudgetNames)
+            {
+                string budgetName = await sqlDataTranslator.GetBudgetNameById(ubnm.BudgetNameId,
+                _config.GetConnectionString("BudgetDataDbConnectionString"));
+
+                dashboardBudgetsTableModel.Add(
+                    new DashboardBudgetsTableModel
+                    {
+                        Id = ubnm.Id,
+                        UserId = ubnm.UserId,
+                        BudgetName = budgetName,
+                        IsDefaultBudget = ubnm.IsDefaultBudget,
+                        Threshhold = ubnm.Threshhold
+                    });
+            }
+
+            return View(dashboardBudgetsTableModel);
         }
     }
 }
---
+++ BudgetBuddyUI/Models/DashboardBudgetsTableModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetBuddyUI.Models
+{
+    public class DashboardBudgetsTableModel
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        [DisplayName("Budget Name")]
+        [RegularExpression(@"^[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(255, MinimumLength = 2)]
+        [Required]
+        public string BudgetName { get; set; } = string.Empty;
+
+        [DisplayName("Default Budget")]
+        public bool IsDefaultBudget { get; set; } = false;
+
+        [Range(0, 999999999999.99)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Threshhold { get; set; }
+    }
+}
